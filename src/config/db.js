2a0d13ddf3c409e2a886021878fbbdc8e3fb81f3
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-07-05 17:19:33 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,9 @@
 // Conexão com o banco de dados
-
-// Pool: é a configuração que faz o login, 
-// asssim não precisa usar login e senha todas as
-// vezes que for fazer querys
 const { Pool } = require('pg');
 
 module.exports = new Pool({
     // user: 'Usuário PostgreSQL',
-    // password: 'Senha PostgreSQL',    
+    // password: 'Senha PostgreSQL',
     host: 'localhost',
     port: 5432,
     database: 'foodfy'
