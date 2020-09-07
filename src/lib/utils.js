700338b7564a46581352369142d9dc732ba787a3
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-09-07 10:21:13 -0300

    Criados lib e config

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -22,5 +22,18 @@ module.exports = {
             birthDay: `${day}/${month}`,
             format: `${day}/${month}/${year}`
         }
+    },
+    randomPassword(length) {
+        let result = '';
+
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+        let charactersLength = characters.length;
+
+        for (let i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+
+        return result;
     }
 };
