ac4d3659d63b9550727cc60778496b555c0b9cd1
Rodrigo Santos <imrodrigosantos@gmail.com>
2020-09-07 10:13:57 -0300

    Scripts de paginação e validação criados

---
+++ public/scripts/pagination-user.js
@@ -0,0 +1,49 @@
+// Paginação
+function paginate(totalPages, selectedPage) {
+    let pages = [], oldPage;
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 1;
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 1;
+        if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push('...');
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1);
+            }
+
+            pages.push(currentPage);
+
+            oldPage = currentPage;
+        }
+    }
+
+    return pages;
+}
+
+function createPagination() {
+    const totalPages = +pagination.dataset.total;
+    const selectedPage = +pagination.dataset.page;
+    const pages = paginate(totalPages, selectedPage);
+
+    let elements = '';
+
+    for (let page of pages) {
+        if (String(page).includes('...')) {
+            elements += `<span>...</span>`;
+        } else {
+            elements += `<a href="?page=${page}" ${(selectedPage == page ? 'class="active"' : '')}>${page}</a>`;
+        }
+    }
+
+    pagination.innerHTML = elements;
+}
+
+const pagination = document.querySelector('.pagination');
+
+if (pagination) {
+    createPagination();
+}
